536ff47249b7263ef9e23880d7e3f64b7588d58e
楚曦 <chuxi@nevernet.com>
2026-03-02 12:26:51 +0800

    chore: 修正sqlglot拼写，版本升级0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nevernet-sql-diff",
-    version="0.1.0",
+    version="0.1.1",
     author="NeverNet",
     author_email="contact@nevernet.com",
     description="SQL Schema Diff and Migration Generator",
